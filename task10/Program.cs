cbae3d9bd3527d87cd4fbfb79cd5fc0615dc7ef9
Nikolay <nlovtsevich@gmail.com>
2023-06-19 13:02:02 +0300

    task10 done

--- task10/Program.cs
+++ task10/Program.cs
@@ -3,5 +3,13 @@
 782 -> 8
 918 -> 1*/
 
-Console.Clear();
-Console.Write("Введите трехзначное число: ");
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    Console.Clear();
+	Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int result = (number % 100) / 10;
+    Console.WriteLine("Вторая цифра данного числа: " + result);
+  }
+}
