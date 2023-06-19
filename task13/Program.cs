1b60aba499bef9fae1de09914b01b2d53ddea50a
Nikolay <nlovtsevich@gmail.com>
2023-06-19 13:19:29 +0300

    task13 done

--- task13/Program.cs
+++ task13/Program.cs
@@ -2,3 +2,27 @@
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6*/
+
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine("Enter a number:");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int digits = 0;
+    int temp = number;
+
+    while (temp != 0) {
+      digits++;
+      temp /= 10;
+    }
+
+    if (digits < 3) {
+      Console.WriteLine("There is no third digit.");
+    } else {
+      int result = (number / (int)Math.Pow(10, digits - 3)) % 10;
+      Console.WriteLine($"The third digit of the given number is: {result}");
+    }
+  }
+}
